ff808d71a1f5340525bddf42c678011cdaf68a87
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-09-26 12:18:41 +0600

    updated country flag and add button

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Country.css';
+
+const Country = ({country}) => {
+
+    const [Visited, setVisited] = useState(false);
+
+    // console.log(country.area.area)
+
+
+    const handleVisited = () =>{
+        
+        //1st way
+
+        // if(Visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        //2nd way
+
+        // setVisited(Visited ? false : true);
+
+        //3rd system
+
+        setVisited(!Visited)
+    }
+
+    return (
+        // <div className= {`country border-lg text-center ${Visited ? 'country-visited' : 'country-not-visited'}`}>
+        <div className= {`country ${Visited && 'country-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+
+            <button onClick={handleVisited}>
+                {Visited ? 'visited' : 'Not visited'}
+            </button>
+            
+        </div>
+    );
+};
+
+export default Country;
